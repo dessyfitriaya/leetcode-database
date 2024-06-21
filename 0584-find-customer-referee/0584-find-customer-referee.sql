3dbc99eeb974a499f05df3990fcd979551ed73fa
Dessy Fitriaya Anwari <103984794+dessyfitriaya@users.noreply.github.com>
2024-06-21 13:50:17 +0800

    Time: 447 ms (99.64%), Space: 0B (100%) - LeetHub

--- 0584-find-customer-referee/0584-find-customer-referee.sql
+++ 0584-find-customer-referee/0584-find-customer-referee.sql
@@ -2,3 +2,15 @@
 SELECT name
 FROM Customer
 WHERE referee_id != 2 OR referee_id IS NULL;
+
+/* versi lain 
+
+SELECT name
+FROM Customer
+WHERE isnull(referee_id, '') != '2'
+
+SELECT name
+FROM Customer
+WHERE id NOT IN (SELECT id FROM Customer WHERE referee_id = 2);
+
+*/
